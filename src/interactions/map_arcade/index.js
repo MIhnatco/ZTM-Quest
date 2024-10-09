df9bb3fa3c5bb5211646983db80f1158fc350113
Ihliana <Ihnatco@protonmail.com>
2024-10-09 11:25:29 +0200

    interactionWithGameMachines

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -1,11 +1,5 @@
 import { enterMapCityInteraction } from './enterMapCity.interactions';
-<<<<<<< HEAD
-
-import {interactionWithJessie} from './jessie.interaction';
-
-=======
 import { interactionWithGameMachine8 } from './game_machine_8';
->>>>>>> bda3a537a25f7769dd8bc32aa5975f9ed7303af0
 import { interactionWithGameMachine2 } from './game_machine_2.interactions';
 import { interactionWithGameMachine4 } from './game_machine_4.interactions';
 import { interactionWithGameMachine6 } from './game_machine_6.interactions';
@@ -23,15 +17,8 @@ const interactions = [
 
     interactionWithGameMachine4,
     interactionWithGameMachine6,
-<<<<<<< HEAD
-
-    interactionWithGameMachine4,
-    interactionWithGameMachine6,
-
-=======
     interactionWithGameMachine8,
->>>>>>> jessieArcade
-];
+g];
 
 export const attachInteractions = (gameObj, k) => {
     const map = k.get('main_map')[0];
